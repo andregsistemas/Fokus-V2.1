b202c2a5f986c03577d76b62ef77d03387794933
dukeossuna <andregsistemas@outlook.com>
2024-02-14 16:01:27 -0300

    Fokus-V2.1 - Projeto Finalizado

--- script-crud.js
+++ script-crud.js
@@ -7,6 +7,7 @@ const ulTarefas = document.querySelector('.app__section-task-list')
 const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
 const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas')
+const btnRemoverTodas = document.querySelector('#btn-remover-todas')
 
 let tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 let tarefaSelecionada = null
@@ -131,11 +132,20 @@ document.addEventListener('FocoFinalizado', () => {
     }
 })
 
-btnRemoverConcluidas.onclick = () => {
-    const seletor = ".app__section-task-list-item-complete"
+const removerTarefas = (somenteCompletas) => {
+    const seletor = somenteCompletas ? ".app__section-task-list-item-complete" : ".app__section-task-list-item"
+    /*
+    let seletor = ".app__section-task-list-item"
+    if (somenteCompletas) {
+        seletor = ".app__section-task-list-item-complete"
+    }
+    */
     document.querySelectorAll(seletor).forEach(elemento => {
         elemento.remove()
     })
-    tarefas = tarefas.filter(e => !e.completa)
+    tarefas = somenteCompletas ? tarefas = tarefas.filter(e => !e.completa) : []
     atualizarTarefa()
 }
+
+btnRemoverConcluidas.onclick = () => removerTarefas(true)
+btnRemoverTodas.onclick = () => removerTarefas(false)
