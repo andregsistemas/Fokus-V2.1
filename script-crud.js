cc28c53f9f87a300743bdf5f2d721baaf763edb8
dukeossuna <andregsistemas@outlook.com>
2024-02-14 15:08:57 -0300

    Fokus-V2.1 - Corrigindo Bugs

--- script-crud.js
+++ script-crud.js
@@ -8,6 +8,7 @@ const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-ta
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 let tarefaSelecionada = null
+let liTarefaSelecionada = null
 
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
 const btnDeletar = document.querySelector('.app__form-footer__button--delete');
@@ -80,10 +81,12 @@ function criarElementoTarefa(tarefa) {
         if (tarefaSelecionada == tarefa) {
             paragrafoDescricaoTarefa.textContent = ''
             tarefaSelecionada = null
+            liTarefaSelecionada = null
             return
         }
-        
+
         tarefaSelecionada = tarefa
+        liTarefaSelecionada = li
         paragrafoDescricaoTarefa.textContent = tarefa.descricao
         li.classList.add('app__section-task-list-item-active')
     }
@@ -112,3 +115,11 @@ tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
 });
+
+document.addEventListener('FocoFinalizado', function () {
+    if (tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', true)
+    }
+})
